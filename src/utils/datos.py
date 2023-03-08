e2187731e4e44981975d505acbb983aec043b44c
Belen Zubillaga <zubillaga_belen@hotmail.com>
2023-03-08 17:19:05 +0100

    Create datos.py

---
+++ src/utils/datos.py
@@ -0,0 +1,3 @@
+path_data = 'data/processed_files/tweets_total.csv' # ruta donde tengo mis datos
+path_final_data = 'data/processed_files/tweets_final.csv' # ruta donde tengo mis datos
+path_lexicon = 'data/spanish_sentiments.csv' # ruta donde tengo mis datos
